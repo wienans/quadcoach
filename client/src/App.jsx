2dde831b7640433fe723fdaa854485dac97f91df
wienans <sven.wienand@outlook.com>
2023-09-03 15:32:31 +0200

    Add Signup Frontend - Private component for hiding pages
    deactivated for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import SignUp from './components/SignUp'
+import PrivateComponent from './components/PrivateComponent'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -11,9 +13,11 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<h1>Exercise component</h1>} />
-          <Route path="/add" element={<h1>add Exercise component</h1>} />
-          <Route path="/edit" element={<h1>edit Exercise component</h1>} />
           <Route path="/signup" element={<SignUp/>} />
+          {/* <Route element={<PrivateComponent/>}> */}
+            <Route path="/add" element={<h1>add Exercise component</h1>} />
+            <Route path="/edit" element={<h1>edit Exercise component</h1>} />
+          {/* </Route> */}
         </Routes>
       </BrowserRouter>
       <Footer/>
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,22 +1,44 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 
-const SignUp=()=>{
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const signUpData=()=>{
-        console.warn(name,email,password)
+const SignUp = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const auth = localStorage.getItem('user')
+        if(auth){
+            navigate('/')
+        }
+    },[])
+
+    const signUpData = async () => {
+        let result = await fetch("http://localhost:3001/register", {
+            method: 'post',
+            body: JSON.stringify({ name, email, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        result = await result.json()
+        localStorage.setItem("user", JSON.stringify(result))
+        navigate("/")
+
+
     }
-    return(
+    return (
         <div className="register">
             <h1>Register</h1>
-            <input className="inputBox" type="text" placeholder="Enter Name" 
-                value={name} onChange={(e)=>setName(e.target.value)}/>
+            <input className="inputBox" type="text" placeholder="Enter Name"
+                value={name} onChange={(e) => setName(e.target.value)} />
             <input className="inputBox" type="text" placeholder="Enter Email"
-                value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                value={email} onChange={(e) => setEmail(e.target.value)} />
             <input className="inputBox" type="password" placeholder="Enter password"
-                value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                value={password} onChange={(e) => setPassword(e.target.value)} />
             <button onClick={signUpData} className="appButton" type="button">Sign Up</button>
         </div>
     )
