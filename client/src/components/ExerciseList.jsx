2fc6cba0681d58edfb404795bb4b2d8a02c54e08
wienans <sven.wienand@outlook.com>
2023-09-17 15:21:29 +0200

    Material UI the Main Pages

--- client/src/components/ExerciseList.jsx
+++ client/src/components/ExerciseList.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom"
+import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack';
 const ExerciseList = () => {
     const [exercises, setExercises] = useState([])
 
     useEffect(() => {
         getExercises()
     }, [])
-
+    const navigate = useNavigate()
     const getExercises = async () => {
         let result = await fetch("/api/exercises")
         result = await result.json()
@@ -25,28 +30,57 @@ const ExerciseList = () => {
             getExercises()
         }
     }
+    const handleRowClick = (
+        params, // GridRowParams
+        event, // MuiEvent<React.MouseEvent<HTMLElement>>
+        details, // GridCallbackDetails
+    ) => {
+        navigate("/exercise/" + params.id)
+    };
+    const columns2 = [
+        {
+            field: 'name',
+            headerName: 'Name',
+            editable: false,
+            hideable: false,
+        },
+        {
+            field: 'persons',
+            headerName: 'Persons',
+            type: 'number',
+            editable: false,
+        },
+        {
+            field: 'tags',
+            headerName: 'Tags',
+            editable: false,
 
+        },
+    ];
+
+    console.log(exercises)
     return (
         <div className="exercise-list">
-            <h3>Exercise List</h3>
-            <input type="" className="search-exercise-box" placeholder="Search Exercise"
-                onChange={searchHandle} />
-            <ul>
-                <li>Name</li>
-                <li>Persons</li>
-                <li>Tags</li>
-            </ul>
-            {
-                exercises.map((item) =>
-                    <ul key={item._id}>
-                        <li><Link to={"/exercise/" + item._id}>{item.name}</Link></li>
-                        <li>{item.persons}</li>
-                        {item.tags != "" && <li>{item.tags.toString()}</li>}
-                        {item.tags == "" && <li>No Tags</li>}
-                    </ul>)
-
-            }
-
+            <Stack spacing={2} sx={{ mt: 3 }}>
+                <Typography variant="h3">Exercise List</Typography>
+                <TextField id="outlined-basic" variant="outlined" placeholder="Search Exercise" onChange={searchHandle} />
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid getRowId={(row) => row._id}
+                        rows={exercises}
+                        columns={columns2}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {
+                                    pageSize: 5,
+                                },
+                            },
+                        }}
+                        pageSizeOptions={[5]}
+                        disableRowSelectionOnClick
+                        onRowClick={handleRowClick}
+                    />
+                </Box>
+            </Stack>
         </div>
     )
 }
