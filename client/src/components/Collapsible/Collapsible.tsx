e88e7e34f300eda6ff554cb36ebb7adced8e082d
wienans <sven.wienand@outlook.com>
2023-10-02 20:42:03 +0200

    Update From Review

--- client/src/components/Collapsible/Collapsible.tsx
+++ client/src/components/Collapsible/Collapsible.tsx
@@ -1,20 +1,20 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Collapse from '@mui/material/Collapse';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {IconButton,IconButtonProps} from '@mui/material';
+import { IconButton, IconButtonProps} from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Typography } from "@mui/material";
 
 
 interface ExpandMoreProps extends IconButtonProps {
-    expand?: boolean;
+    expand: boolean;
 }
 export type CollapsibleProps  = {
-    label?: string,
-    children?: string
+    label: string,
+    children: string
 }
 const ExpandMore = styled((props: ExpandMoreProps) => {
     const {expand, ...other } = props;
@@ -32,7 +32,7 @@ const Collapsible = (props: CollapsibleProps) => {
     const toggle = () => {
         setOpen(!open);
     };
-    useRef();
+
     return (
         <div>
             <Card>
