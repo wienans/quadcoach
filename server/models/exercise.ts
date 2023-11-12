7a62c332c418e8df393a2596813ba7710042c024
wienans <sven.wienand@outlook.com>
2023-11-12 16:44:37 +0100

    Server Formating

--- server/models/exercise.ts
+++ server/models/exercise.ts
@@ -1,83 +1,85 @@
-import {Schema,model,Types} from "mongoose";
-
+import { Schema, model, Types } from "mongoose";
 
 interface IBlock {
-    video_url?: string;
-    description?: string;
-    coaching_points?: string;
-    tactics_board?: Types.ObjectId;
-    time_min?: number;
+  video_url?: string;
+  description?: string;
+  coaching_points?: string;
+  tactics_board?: Types.ObjectId;
+  time_min?: number;
 }
 
 interface IExercise {
-    name: string;
-    materials?: Types.Array<string>;
-    time_min: number;
-    beaters?: number;
-    chasers?: number;
-    persons: number;
-    tags?: Types.Array<string>;
-    coaching_points?: string; // unused for block model only backwards compatible
-    creator?: string;
-    description_blocks?: Types.DocumentArray<IBlock>;
-    related_to?: Types.Array<Types.ObjectId>;
+  name: string;
+  materials?: Types.Array<string>;
+  time_min: number;
+  beaters?: number;
+  chasers?: number;
+  persons: number;
+  tags?: Types.Array<string>;
+  coaching_points?: string; // unused for block model only backwards compatible
+  creator?: string;
+  description_blocks?: Types.DocumentArray<IBlock>;
+  related_to?: Types.Array<Types.ObjectId>;
 }
 
 const blockSchema = new Schema<IBlock>({
-    video_url: {
-        type: String
-    },
-    description:{
-        type: String
-    },
-    coaching_points: {
-        type: String
-    },
-    tactics_board: {
-        type: Types.ObjectId,
-        ref: 'tacticBoards'
-    },
-    time_min: {
-        type: Number
-    }
-})
+  video_url: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  coaching_points: {
+    type: String,
+  },
+  tactics_board: {
+    type: Types.ObjectId,
+    ref: "tacticBoards",
+  },
+  time_min: {
+    type: Number,
+  },
+});
 
-const exerciseSchema = new Schema<IExercise>({
+const exerciseSchema = new Schema<IExercise>(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     materials: {
-        type: [String]
+      type: [String],
     },
-    time_min:{
-        type: Number
+    time_min: {
+      type: Number,
     },
     beaters: {
-        type: Number
+      type: Number,
     },
     chasers: {
-        type: Number
+      type: Number,
     },
     persons: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     tags: {
-        type: [String]
+      type: [String],
     },
     creator: {
-        type: String
+      type: String,
     },
     description_blocks: {
-        type: [blockSchema]
+      type: [blockSchema],
     },
     related_to: {
-        type: [Types.ObjectId],
-        ref: 'exercises'
+      type: [Types.ObjectId],
+      ref: "exercises",
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-const Exercise = model<IExercise>('exercises', exerciseSchema);
+const Exercise = model<IExercise>("exercises", exerciseSchema);
 
 export default Exercise;
