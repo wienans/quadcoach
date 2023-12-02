1f9701dd7af62e91fb14cd46ce89cd83e1edfab9
wienans <sven.wienand@outlook.com>
2023-12-02 13:59:29 +0100

    Get the Background Canvas to work

--- client/src/pages/TacticsBoard/TacticsBoard.tsx
+++ client/src/pages/TacticsBoard/TacticsBoard.tsx
@@ -1,15 +1,35 @@
 import "./translations";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUpdateBreadcrumbs } from "../../components/Layout/hooks";
 import { useTranslation } from "react-i18next";
 import { SoftTypography, SoftInput, SoftButton } from "../../components";
+import { fabric } from "fabric";
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+
 const TacticsBoard = (): JSX.Element => {
   const { t } = useTranslation("TacticsBoard");
   useUpdateBreadcrumbs(t("TacticsBoard:titel"));
+
+  const { editor, onReady } = useFabricJSEditor();
+
+  useEffect(() => {
+    if (!editor || !fabric) {
+      return;
+    }
+    editor.canvas.setHeight(686);
+    editor.canvas.setWidth(1220);
+    editor.canvas.setBackgroundImage(
+      "/full-court_inkscape.svg",
+      editor.canvas.renderAll.bind(editor.canvas),
+    );
+    editor.canvas.renderAll();
+  }, [editor?.canvas.backgroundImage]);
+
   return (
     <p>
       <SoftTypography variant="h3">{t("TacticsBoard:titel")}</SoftTypography>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
     </p>
   );
 };
