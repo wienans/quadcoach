2b1fdc5824d07a70dce772257cec3d9a0b4e2570
wienans <sven.wienand@outlook.com>
2023-12-19 16:10:24 +0100

    Update Tacticboard to first working prototype

--- client/src/components/TacticsBoardToolBar/TacticsBoardToolBar.tsx
+++ client/src/components/TacticsBoardToolBar/TacticsBoardToolBar.tsx
@@ -1,4 +1,5 @@
 import { useFabricJs } from "../FabricJsContext";
+import { useState } from "react";
 import { SoftTypography, SoftButton, SoftBox } from "..";
 import {
   Switch,
@@ -9,30 +10,41 @@ import {
 } from "@mui/material";
 
 export type TacticsBoardToolBarProps = {
+  editMode: boolean;
+  disabled?: boolean;
   setEditMode: (editMode: boolean) => void;
   setPage: (page: number) => void;
   currentPage: number;
   setMaxPages: (maxPages: number) => void;
   maxPages: number;
+  onSave: () => void;
+  onLoadPage: (page: number, newPage?: boolean, removePage?: boolean) => void;
 };
 
 const TacticsBoardToolBar = ({
+  editMode,
   setEditMode,
   setPage,
   currentPage,
   setMaxPages,
   maxPages,
+  onSave,
+  disabled,
+  onLoadPage,
 }: TacticsBoardToolBarProps): JSX.Element => {
   const { getAllObjectsJson, removeActiveObjects, setSelection, setDrawMode } =
     useFabricJs();
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
+    onLoadPage(value);
   };
+  const [drawingEnabled, enableDrawing] = useState<boolean>(false);
   return (
     <div>
       <Grid container>
         <Grid item xs={3}>
           <SoftButton
+            disabled={disabled}
             onClick={() => {
               removeActiveObjects();
             }}
@@ -41,22 +53,21 @@ const TacticsBoardToolBar = ({
           </SoftButton>
         </Grid>
         <Grid item xs={3}>
-          <SoftButton
-            onClick={() => {
-              console.log(getAllObjectsJson());
-            }}
-          >
-            Save
-          </SoftButton>
+          <SoftButton onClick={onSave}>Save</SoftButton>
         </Grid>
         <Grid item xs={2}>
           <FormControlLabel
             control={
               <Switch
                 defaultChecked
+                disabled={disabled}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                   setEditMode(event.target.checked);
                   setSelection(event.target.checked);
+                  if (event.target.checked == false) {
+                    enableDrawing(false);
+                    setDrawMode(false);
+                  }
                 }}
               />
             }
@@ -65,8 +76,11 @@ const TacticsBoardToolBar = ({
           <FormControlLabel
             control={
               <Switch
+                checked={drawingEnabled}
+                disabled={!editMode || disabled}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                   setDrawMode(event.target.checked);
+                  enableDrawing(event.target.checked);
                 }}
               />
             }
@@ -77,9 +91,12 @@ const TacticsBoardToolBar = ({
           <Stack spacing={2} direction="row">
             <SoftButton
               size="small"
+              disabled={disabled || maxPages == 1 || currentPage != maxPages}
               onClick={() => {
                 if (maxPages > 1) {
                   setMaxPages(maxPages - 1);
+                  setPage(currentPage - 1);
+                  onLoadPage(currentPage - 1, false, true);
                 }
               }}
             >
@@ -90,12 +107,16 @@ const TacticsBoardToolBar = ({
               siblingCount={0}
               boundaryCount={0}
               page={currentPage}
+              disabled={disabled}
               onChange={handleChange}
             />
             <SoftButton
               size="small"
+              disabled={disabled || currentPage != maxPages}
               onClick={() => {
                 setMaxPages(maxPages + 1);
+                setPage(currentPage + 1);
+                onLoadPage(currentPage + 1, true, false);
               }}
             >
               +
--- client/src/pages/UpdateTacticBoard/UpdateTacticBoard.tsx
+++ client/src/pages/UpdateTacticBoard/UpdateTacticBoard.tsx
@@ -4,7 +4,12 @@ import { useState, useEffect, useRef } from "react";
 import { Alert, Grid, Skeleton } from "@mui/material";
 import { useUpdateBreadcrumbs } from "../../components/Layout/hooks";
 import { useTranslation } from "react-i18next";
-import { useGetTacticBoardQuery } from "../../pages/tacticboardApi";
+import {
+  useAddTacticBoardMutation,
+  useUpdateTacticBoardMutation,
+  useGetTacticBoardQuery,
+} from "../../pages/tacticboardApi";
+import { TacticBoard } from "../../api/quadcoachApi/domain";
 import {
   SoftTypography,
   SoftBox,
@@ -14,11 +19,12 @@ import {
   TacticsBoardSpeedDialBalls,
 } from "../../components";
 import { useFabricJs } from "../../components/FabricJsContext/useFabricJs";
+import cloneDeep from "lodash/cloneDeep";
 
 const UpdateTacticBoard = (): JSX.Element => {
   const { t } = useTranslation("UpdateTacticBoard");
   const { id: tacticBoardId } = useParams();
-  const { addObject } = useFabricJs();
+  const { getAllObjectsJson, loadFromJson } = useFabricJs();
   useUpdateBreadcrumbs(t("UpdateTacticBoard:titel"));
 
   const {
@@ -29,75 +35,130 @@ const UpdateTacticBoard = (): JSX.Element => {
     skip: tacticBoardId == null,
   });
 
+  const [
+    updateTacticBoard,
+    {
+      isError: isUpdateTacticBoardError,
+      isLoading: isUpdateTacticBoardLoading,
+      isSuccess: isUpdateTacticBoardSuccess,
+    },
+  ] = useUpdateTacticBoardMutation();
+
   const refContainer = useRef<HTMLDivElement>(null);
   const [editMode, setEditMode] = useState<boolean>(true);
-  const [page, setPage] = useState<number>(1);
+  const [currentPage, setPage] = useState<number>(1);
   const [maxPages, setMaxPages] = useState<number>(1);
+  const [tacticBoardState, setTacticBoardState] = useState<TacticBoard>();
 
+  // Setting Tacticboard to State
+  useEffect(() => {
+    setTacticBoardState(tacticBoard);
+  }, [tacticBoard]);
+
+  const onLoadPage = (
+    page: number,
+    newPage?: boolean,
+    removePage?: boolean,
+  ) => {
+    if (newPage && removePage) return;
+    let updatedTacticBoard: TacticBoard = cloneDeep(tacticBoardState);
+    if (newPage) {
+      // New Page
+      updatedTacticBoard.pages[page - 1] = getAllObjectsJson();
+      updateTacticBoard(updatedTacticBoard);
+    }
+    if (removePage) {
+      // Remove Last Page
+      console.log("remove");
+      updatedTacticBoard.pages.pop();
+      updateTacticBoard(updatedTacticBoard);
+    }
+    // Visit Page
+    console.log(page);
+    loadFromJson(updatedTacticBoard.pages[page - 1]);
+  };
+
+  const onSave = () => {
+    if (!tacticBoardState) return;
+    console.log(getAllObjectsJson());
+    let updatedTacticBoard: TacticBoard = cloneDeep(tacticBoardState);
+    updatedTacticBoard.pages[currentPage - 1] = getAllObjectsJson();
+    updateTacticBoard(updatedTacticBoard);
+  };
   return (
     <div>
-      {isTacticBoardError && (
+      {isTacticBoardError ? (
         <Grid item xs={12} justifyContent="center" display="flex">
           <Alert color="error">{"Error"}</Alert>
         </Grid>
-      )}
-      <SoftBox
-        variant="contained"
-        shadow="lg"
-        opacity={1}
-        p={1}
-        my={2}
-        borderRadius="lg"
-      >
-        <SoftTypography variant="h3">
-          {t("UpdateTacticBoard:titel")}
-        </SoftTypography>
-      </SoftBox>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TacticsBoardToolBar
-              setEditMode={setEditMode}
-              setPage={setPage}
-              currentPage={page}
-              setMaxPages={setMaxPages}
-              maxPages={maxPages}
-            />
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            ref={refContainer}
-            style={{ display: "flex", justifyContent: "center" }}
+      ) : (
+        <>
+          <SoftBox
+            variant="contained"
+            shadow="lg"
+            opacity={1}
+            p={1}
+            my={2}
+            borderRadius="lg"
           >
-            <TacticsBoardSpeedDialBalls editMode={editMode} />
-            <TacticsBoardSpeedDial teamB={false} editMode={editMode} />
-            {isTacticBoardLoading ? (
-              <Skeleton variant="rectangular" width={1220} height={686} />
-            ) : (
-              <FabricJsCanvas
-                initialHight={686}
-                initialWidth={1220}
-                backgroundImage="/full-court_inkscape.svg"
-                containerRef={refContainer}
-              />
-            )}
-            <TacticsBoardSpeedDial teamB={true} editMode={editMode} />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            style={{ display: "flex", justifyContent: "center" }}
-          ></Grid>
-        </Grid>
-      </div>
+            <SoftTypography variant="h3">
+              {t("UpdateTacticBoard:titel")}
+            </SoftTypography>
+          </SoftBox>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TacticsBoardToolBar
+                  editMode={editMode}
+                  setEditMode={setEditMode}
+                  setPage={setPage}
+                  currentPage={currentPage}
+                  setMaxPages={setMaxPages}
+                  maxPages={maxPages}
+                  onSave={onSave}
+                  onLoadPage={onLoadPage}
+                  disabled={isTacticBoardLoading}
+                />
+              </Grid>
+
+              <Grid
+                item
+                xs={12}
+                ref={refContainer}
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                <TacticsBoardSpeedDialBalls editMode={editMode} />
+                <TacticsBoardSpeedDial teamB={false} editMode={editMode} />
+                {isTacticBoardLoading ? (
+                  <Skeleton variant="rectangular" width={1220} height={686} />
+                ) : (
+                  <>
+                    <FabricJsCanvas
+                      initialHight={686}
+                      initialWidth={1220}
+                      backgroundImage="/full-court_inkscape.svg"
+                      containerRef={refContainer}
+                      initialCanvas={tacticBoard?.pages[currentPage - 1]}
+                    />
+                  </>
+                )}
+                <TacticsBoardSpeedDial teamB={true} editMode={editMode} />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                style={{ display: "flex", justifyContent: "center" }}
+              ></Grid>
+            </Grid>
+          </div>
+        </>
+      )}
     </div>
   );
 };
