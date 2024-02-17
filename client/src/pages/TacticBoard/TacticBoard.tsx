a81727e3b1d03fcd1aa53055bca5f7809aad5862
JoramStiel <joram.stiel@gmx.de>
2024-02-17 15:24:33 +0100

    Add DashboardLayout to TacticsBoard

--- client/src/pages/TacticBoard/TacticBoard.tsx
+++ client/src/pages/TacticBoard/TacticBoard.tsx
@@ -2,11 +2,20 @@ import "./translations";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect, useRef, useCallback } from "react";
-import { Alert, Grid, Pagination, Skeleton } from "@mui/material";
+import {
+  Alert,
+  Box,
+  ButtonGroup,
+  Card,
+  CardActions,
+  CardHeader,
+  Grid,
+  Pagination,
+  Skeleton,
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import {
   SoftTypography,
-  SoftBox,
   FabricJsCanvas,
   SoftButton,
 } from "../../components";
@@ -18,7 +27,82 @@ import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import MovieIcon from "@mui/icons-material/Movie";
 import "../fullscreen.css";
+import { DashboardLayout } from "../../components/LayoutContainers";
+import { TacticBoard } from "../../api/quadcoachApi/domain";
 let mediaRecorder: MediaRecorder;
+
+type TacticBoardActionsProps = {
+  tacticBoard: TacticBoard | undefined;
+  isAnimating: boolean;
+  isRecording: boolean;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+  onEditTacticBoardClick: () => void;
+  onAnimateClick: () => void;
+  onRecordClick: () => void;
+  onFullScreenClick: () => void;
+};
+
+const TacticBoardActions = ({
+  tacticBoard,
+  isAnimating,
+  isRecording,
+  currentPage,
+  onPageChange,
+  onEditTacticBoardClick,
+  onAnimateClick,
+  onRecordClick,
+  onFullScreenClick,
+}: TacticBoardActionsProps): JSX.Element => (
+  <CardActions
+    disableSpacing
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-start",
+    }}
+  >
+    <Pagination
+      count={tacticBoard?.pages.length}
+      siblingCount={0}
+      disabled={isAnimating || isRecording}
+      page={currentPage}
+      onChange={(_, value: number) => onPageChange(value)}
+      sx={{ mr: 1 }}
+    />
+    <ButtonGroup size="small" aria-label="Small button group">
+      <SoftButton
+        iconOnly={true}
+        disabled={isAnimating || isRecording}
+        onClick={onEditTacticBoardClick}
+      >
+        <EditIcon />
+      </SoftButton>
+      <SoftButton
+        iconOnly={true}
+        disabled={isRecording}
+        onClick={onAnimateClick}
+      >
+        {isAnimating ? <PauseCircleIcon /> : <PlayCircleIcon />}
+      </SoftButton>
+      <SoftButton
+        iconOnly={true}
+        disabled={isAnimating || isRecording}
+        onClick={onRecordClick}
+      >
+        <MovieIcon />
+      </SoftButton>
+      <SoftButton
+        iconOnly={true}
+        disabled={isAnimating || isRecording}
+        onClick={onFullScreenClick}
+      >
+        <FullscreenIcon />
+      </SoftButton>
+    </ButtonGroup>
+  </CardActions>
+);
+
 const TacticsBoard = (): JSX.Element => {
   const { t } = useTranslation("TacticBoard");
   const { id: tacticBoardId } = useParams();
@@ -37,6 +121,7 @@ const TacticsBoard = (): JSX.Element => {
   const [currentPage, setPage] = useState<number>(1);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
   const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
 
   const onLoadPage = useCallback(
     (page: number) => {
@@ -48,7 +133,7 @@ const TacticsBoard = (): JSX.Element => {
     [loadFromJson, setSelection, tacticBoard],
   );
 
-  const handleFullScreen = () => {
+  const onFullScreenClick = () => {
     const container = refFullScreenContainer.current;
     const isFullscreen = document.fullscreenElement;
     if (isFullscreen) {
@@ -82,6 +167,38 @@ const TacticsBoard = (): JSX.Element => {
     document.body.appendChild(tag);
     tag.click();
     document.body.removeChild(tag);
+  };
+
+  const onEditTacticBoardClick = () => {
+    navigate(`/tacticboards/${tacticBoardId}/update`);
+  };
+
+  const onAnimateClick = () => {
+    setIsAnimating(!isAnimating);
+  };
+
+  const onRecordClick = () => {
+    const canvasStream = canvas?.lowerCanvasEl.captureStream(60);
+    mediaRecorder = new MediaRecorder(canvasStream, {
+      mimeType: "video/webm",
+    });
+    let chunks: Blob[] = [];
+
+    mediaRecorder.onstop = function () {
+      downloadVideo(chunks);
+      chunks = [];
+    };
+
+    mediaRecorder.ondataavailable = function (e) {
+      chunks.push(e.data);
+    };
+    mediaRecorder.start();
+    setIsRecording(!isRecording);
+  };
+
+  const onPageChange = (page: number) => {
+    onLoadPage(page);
+    setPage(page);
   };
 
   useEffect(() => {
@@ -178,29 +295,90 @@ const TacticsBoard = (): JSX.Element => {
     return () => clearInterval(interval);
   }, [isRecording, onLoadPage, tacticBoard, getAllObjects, canvas]);
 
-  return (
-    <div>
-      <SoftBox
-        variant="contained"
-        shadow="lg"
-        opacity={1}
-        p={1}
-        my={2}
-        borderRadius="lg"
-      >
-        <SoftTypography variant="h3">
-          {t("TacticBoard:titel") +
-            ": " +
-            (tacticBoard ? tacticBoard.name : "")}
-        </SoftTypography>
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      setIsFullScreen(document.fullscreenElement != null);
+    };
 
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
+
+  return (
+    <DashboardLayout
+      header={(scrollTrigger) => (
+        <Card
+          sx={(theme) => ({
+            position: "sticky",
+            top: theme.spacing(1),
+            zIndex: 1,
+            ...(scrollTrigger
+              ? {
+                  backgroundColor: theme.palette.transparent.main,
+                  boxShadow: theme.boxShadows.navbarBoxShadow,
+                  backdropFilter: `saturate(200%) blur(${theme.functions.pxToRem(
+                    30,
+                  )})`,
+                }
+              : {
+                  backgroundColor: theme.functions.rgba(
+                    theme.palette.white.main,
+                    0.8,
+                  ),
+                  boxShadow: "none",
+                  backdropFilter: "none",
+                }),
+            transition: theme.transitions.create("all", {
+              easing: theme.transitions.easing.easeInOut,
+              duration: theme.transitions.duration.standard,
+            }),
+          })}
         >
+          <CardHeader
+            title={
+              <SoftTypography variant="h3">
+                {`${t("TacticBoard:titel")}${
+                  tacticBoard ? `: ${tacticBoard.name}` : ""
+                }`}
+              </SoftTypography>
+            }
+          />
+          {!isTacticBoardError && !isTacticBoardLoading && (
+            <TacticBoardActions
+              currentPage={currentPage}
+              isAnimating={isAnimating}
+              isRecording={isRecording}
+              onAnimateClick={onAnimateClick}
+              onEditTacticBoardClick={onEditTacticBoardClick}
+              onFullScreenClick={onFullScreenClick}
+              onPageChange={onPageChange}
+              onRecordClick={onRecordClick}
+              tacticBoard={tacticBoard}
+            />
+          )}
+        </Card>
+      )}
+    >
+      {() => (
+        <Box ref={refFullScreenContainer}>
+          {isFullScreen && !isTacticBoardLoading && (
+            <Card>
+              <TacticBoardActions
+                currentPage={currentPage}
+                isAnimating={isAnimating}
+                isRecording={isRecording}
+                onAnimateClick={onAnimateClick}
+                onEditTacticBoardClick={onEditTacticBoardClick}
+                onFullScreenClick={onFullScreenClick}
+                onPageChange={onPageChange}
+                onRecordClick={onRecordClick}
+                tacticBoard={tacticBoard}
+              />
+            </Card>
+          )}
           {isTacticBoardError && (
             <Grid item xs={12} justifyContent="center" display="flex">
               <Alert color="error">{"Error"}</Alert>
@@ -210,111 +388,24 @@ const TacticsBoard = (): JSX.Element => {
             <Skeleton variant="rectangular" width={"100%"} height={100} />
           )}
           {!isTacticBoardError && !isTacticBoardLoading && (
-            <Grid container spacing={2} ref={refFullScreenContainer}>
-              <Grid
-                item
-                xs={12}
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                <Grid
-                  item
-                  xs={6}
-                  style={{ display: "flex", justifyContent: "center" }}
-                >
-                  <Pagination
-                    count={tacticBoard?.pages.length}
-                    siblingCount={0}
-                    disabled={isAnimating || isRecording}
-                    page={currentPage}
-                    onChange={(_, value: number) => {
-                      onLoadPage(value);
-                      setPage(value);
-                    }}
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={4}
-                  style={{ display: "flex", justifyContent: "center" }}
-                >
-                  <SoftButton
-                    iconOnly={true}
-                    disabled={isAnimating || isRecording}
-                    onClick={() => {
-                      navigate(`/tacticboards/${tacticBoardId}/update`);
-                    }}
-                  >
-                    <EditIcon />
-                  </SoftButton>
-                </Grid>
-                <Grid item xs={2}>
-                  <SoftButton
-                    iconOnly={true}
-                    disabled={isRecording}
-                    onClick={() => {
-                      setIsAnimating(!isAnimating);
-                    }}
-                  >
-                    {isAnimating ? <PauseCircleIcon /> : <PlayCircleIcon />}
-                  </SoftButton>
-                </Grid>
-                <Grid item xs={2}>
-                  <SoftButton
-                    iconOnly={true}
-                    disabled={isAnimating || isRecording}
-                    onClick={() => {
-                      const canvasStream =
-                        canvas?.lowerCanvasEl.captureStream(60);
-                      mediaRecorder = new MediaRecorder(canvasStream, {
-                        mimeType: "video/webm",
-                      });
-                      let chunks: Blob[] = [];
-
-                      mediaRecorder.onstop = function () {
-                        downloadVideo(chunks);
-                        chunks = [];
-                      };
-
-                      mediaRecorder.ondataavailable = function (e) {
-                        chunks.push(e.data);
-                      };
-                      mediaRecorder.start();
-                      setIsRecording(!isRecording);
-                    }}
-                  >
-                    <MovieIcon />
-                  </SoftButton>
-                </Grid>
-                <Grid item xs={2}>
-                  <SoftButton
-                    iconOnly={true}
-                    disabled={isAnimating || isRecording}
-                    onClick={handleFullScreen}
-                  >
-                    <FullscreenIcon />
-                  </SoftButton>
-                </Grid>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                ref={refContainer}
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <FabricJsCanvas
-                  initialHight={686}
-                  initialWidth={1220}
-                  containerRef={refContainer}
-                />
-              </Grid>
-            </Grid>
+            <Box
+              ref={refContainer}
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <FabricJsCanvas
+                initialHight={686}
+                initialWidth={1220}
+                containerRef={refContainer}
+              />
+            </Box>
           )}
-        </div>
-      </SoftBox>
-    </div>
+        </Box>
+      )}
+    </DashboardLayout>
   );
 };
 
