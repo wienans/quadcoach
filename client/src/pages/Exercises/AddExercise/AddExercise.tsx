ab99849dce56f877fb1bb85bbd199dc9dec7368d
JoramStiel <joram.stiel@gmx.de>
2024-02-17 23:50:30 +0100

    - Add DashboardLayout to AddExercise

--- client/src/pages/Exercises/AddExercise/AddExercise.tsx
+++ client/src/pages/Exercises/AddExercise/AddExercise.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Grid } from "@mui/material";
+import { Card, CardHeader, Grid } from "@mui/material";
 import { SoftBox, SoftButton, SoftTypography } from "../../../components";
 import ExerciseEditForm from "../../../components/ExerciseEditForm";
 import { ExerciseWithOutId } from "../../../api/quadcoachApi/domain";
@@ -7,6 +7,7 @@ import { useEffect } from "react";
 import { useAddExerciseMutation } from "../../exerciseApi";
 import { useTranslation } from "react-i18next";
 import "./translations";
+import { DashboardLayout } from "../../../components/LayoutContainers";
 
 const AddExercise = () => {
   const { t } = useTranslation("AddExercise");
@@ -32,34 +33,71 @@ const AddExercise = () => {
   };
 
   return (
-    <ExerciseEditForm
-      extraRows={() => (
-        <>
-          {isAddExerciseError && (
-            <Grid item xs={12} justifyContent="center" display="flex">
-              <SoftBox bgColor="error">
-                {t("AddExercise:errorWhileAdding")}
-              </SoftBox>
-            </Grid>
-          )}
-          <Grid item xs={12} justifyContent="center" display="flex">
-            <SoftButton
-              type="submit"
-              color="primary"
-              disabled={isAddExerciseLoading}
-            >
-              {t("AddExercise:addExcercise")}
-            </SoftButton>
-          </Grid>
-        </>
+    <DashboardLayout
+      header={(scrollTrigger) => (
+        <Card
+          sx={(theme) => ({
+            position: "sticky",
+            top: theme.spacing(1),
+            zIndex: 1,
+            ...(scrollTrigger
+              ? {
+                  backgroundColor: theme.palette.transparent.main,
+                  boxShadow: theme.boxShadows.navbarBoxShadow,
+                  backdropFilter: `saturate(200%) blur(${theme.functions.pxToRem(
+                    30,
+                  )})`,
+                }
+              : {
+                  backgroundColor: theme.functions.rgba(
+                    theme.palette.white.main,
+                    0.8,
+                  ),
+                  boxShadow: "none",
+                  backdropFilter: "none",
+                }),
+            transition: theme.transitions.create("all", {
+              easing: theme.transitions.easing.easeInOut,
+              duration: theme.transitions.duration.standard,
+            }),
+          })}
+        >
+          <CardHeader
+            title={
+              <SoftTypography variant="h3">
+                {t("AddExercise:addExcercise")}
+              </SoftTypography>
+            }
+          />
+        </Card>
       )}
-      header={
-        <SoftTypography variant="h3">
-          {t("AddExercise:addExcercise")}
-        </SoftTypography>
-      }
-      onSubmit={onSubmit}
-    />
+    >
+      {() => (
+        <ExerciseEditForm
+          extraRows={() => (
+            <>
+              {isAddExerciseError && (
+                <Grid item xs={12} justifyContent="center" display="flex">
+                  <SoftBox bgColor="error">
+                    {t("AddExercise:errorWhileAdding")}
+                  </SoftBox>
+                </Grid>
+              )}
+              <Grid item xs={12} justifyContent="center" display="flex">
+                <SoftButton
+                  type="submit"
+                  color="primary"
+                  disabled={isAddExerciseLoading}
+                >
+                  {t("AddExercise:addExcercise")}
+                </SoftButton>
+              </Grid>
+            </>
+          )}
+          onSubmit={onSubmit}
+        />
+      )}
+    </DashboardLayout>
   );
 };
 
