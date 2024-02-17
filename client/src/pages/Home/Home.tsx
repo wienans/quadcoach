7ed6652b83e0b34c8ccd9359039708991dcbd81d
JoramStiel <joram.stiel@gmx.de>
2024-02-18 00:15:56 +0100

    Add DashboardLayout to Home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,97 +1,94 @@
 import "./translations";
-import { useParams, useNavigate, Link } from "react-router-dom";
-import { useEffect } from "react";
-import {
-  Alert,
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Grid,
-  LinearProgress,
-  Skeleton,
-} from "@mui/material";
-import { SoftBox, SoftButton, SoftTypography } from "../../components";
-import { Chip } from "@mui/material";
-import CopyrightIcon from "@mui/icons-material/Copyright";
-import ReactPlayer from "react-player";
-import { Exercise } from "../../api/quadcoachApi/domain";
-import {
-  useDeleteExerciseMutation,
-  useGetExerciseQuery,
-  useGetRelatedExercisesQuery,
-} from "../exerciseApi";
+import { Card } from "@mui/material";
+import { SoftBox, SoftTypography } from "../../components";
 import { useTranslation } from "react-i18next";
 
 import logo from "../../../public/logo.svg";
+import { DashboardLayout } from "../../components/LayoutContainers";
 
 const Home = () => {
   const { t } = useTranslation("Home");
 
   return (
-    <div>
-      <SoftBox
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <img
-          src={logo}
-          alt="Logo"
-          style={{ width: "250px", margin: "50px 0" }}
-        />
-      </SoftBox>
-      <SoftBox>
-        <Card sx={{ height: "100%" }}>
-          <SoftBox p={2}>
-            <SoftBox display="flex" justifyContent="space-between" width="100%">
-              <SoftTypography
-                variant="h3"
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                Beta Tesing
-              </SoftTypography>
-              <SoftTypography
-                variant="h3"
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                05.Feb.2024
-              </SoftTypography>
-            </SoftBox>
-            <SoftTypography>
-              As Login is implemented we can do the fist Beta Testing of the App
-              Quadcoach
-            </SoftTypography>
+    <DashboardLayout>
+      {() => (
+        <>
+          <SoftBox
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "250px", margin: "50px 0" }}
+            />
           </SoftBox>
-        </Card>
-      </SoftBox>
-      <SoftBox mt={5} mb={3}>
-        <Card sx={{ height: "100%" }}>
-          <SoftBox p={2}>
-            <SoftBox display="flex" justifyContent="space-between" width="100%">
-              <SoftTypography
-                variant="h3"
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                Alpha Testing
-              </SoftTypography>
-              <SoftTypography
-                variant="h3"
-                fontWeight="bold"
-                textTransform="uppercase"
-              >
-                04.Feb.2024
-              </SoftTypography>
-            </SoftBox>
-            <SoftTypography>
-              Local Alpha Testing works well and we are constantly adding new
-              features
-            </SoftTypography>
+          <SoftBox>
+            <Card sx={{ height: "100%" }}>
+              <SoftBox p={2}>
+                <SoftBox
+                  display="flex"
+                  justifyContent="space-between"
+                  width="100%"
+                >
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
+                    Beta Tesing
+                  </SoftTypography>
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
+                    05.Feb.2024
+                  </SoftTypography>
+                </SoftBox>
+                <SoftTypography>
+                  As Login is implemented we can do the fist Beta Testing of the
+                  App Quadcoach
+                </SoftTypography>
+              </SoftBox>
+            </Card>
           </SoftBox>
-        </Card>
-      </SoftBox>
-    </div>
+          <SoftBox mt={5} mb={3}>
+            <Card sx={{ height: "100%" }}>
+              <SoftBox p={2}>
+                <SoftBox
+                  display="flex"
+                  justifyContent="space-between"
+                  width="100%"
+                >
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
+                    Alpha Testing
+                  </SoftTypography>
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
+                    04.Feb.2024
+                  </SoftTypography>
+                </SoftBox>
+                <SoftTypography>
+                  Local Alpha Testing works well and we are constantly adding
+                  new features
+                </SoftTypography>
+              </SoftBox>
+            </Card>
+          </SoftBox>
+        </>
+      )}
+    </DashboardLayout>
   );
 };
 
