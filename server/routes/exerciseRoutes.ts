5d40bebd1e7233ac4f99a7695557c560e7526344
wienans <sven.wienand@outlook.com>
2024-04-07 14:11:17 +0200

    Exercise Access from Backend

--- server/routes/exerciseRoutes.ts
+++ server/routes/exerciseRoutes.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import * as exerciseController from "../controllers/exerciseController";
-
+import verifyJWT from "../middleware/verifyJWT";
 const router = express.Router();
 
 router
   .route("/")
   .get(exerciseController.getAllExercises)
-  .post(exerciseController.createNewExercise);
+  .post(verifyJWT, exerciseController.createNewExercise);
 
 router
   .route("/:id")
   .get(exerciseController.getById)
-  .put(exerciseController.updateById)
-  .delete(exerciseController.deleteById);
+  .put(verifyJWT, exerciseController.updateById)
+  .delete(verifyJWT, exerciseController.deleteById);
 
 router.route("/:id/relatedExercises").get(exerciseController.getRelatedById);
 
