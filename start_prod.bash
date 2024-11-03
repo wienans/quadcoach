80fe06c5b2fd142261da07b43b3dec56049b1514
wienans <sven.wienand@outlook.com>
2024-11-03 13:24:26 +0100

    Update start script

--- start_prod.bash
+++ start_prod.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker compose -f docker-compose.dev down
+docker compose -f docker-compose.prod stop
 docker rm quadcoach-mongodb quadcoach-backend quadcoach-frontend
 docker image rm quadcoach-app
 docker compose -f docker-compose.prod up --build -d
