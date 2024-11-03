835fc2b8f1c5ef1084bc3adf28e4510c9452d353
wienans <sven.wienand@outlook.com>
2024-11-03 12:00:03 +0100

    Detach the production console

--- start_prod.bash
+++ start_prod.bash
@@ -2,4 +2,4 @@
 docker compose -f docker-compose.dev down
 docker rm quadcoach-mongodb quadcoach-backend quadcoach-frontend
 docker image rm quadcoach-app
-docker compose -f docker-compose.prod up --build
+docker compose -f docker-compose.prod up --build -d
