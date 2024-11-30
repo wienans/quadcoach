4c9e61935dc644fb20df47cd1b67e059711731e2
wienans <sven.wienand@outlook.com>
2024-11-30 17:56:41 +0100

    Remove Side Nave Delete User

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -4,37 +4,14 @@ import HomeIcon from "@mui/icons-material/Home";
 import DeveloperBoardIcon from "@mui/icons-material/DeveloperBoard";
 import ListIcon from "@mui/icons-material/List";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import DeleteIcon from "@mui/icons-material/Delete";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LoginIcon from "@mui/icons-material/Login";
 import { PresistLogin } from "..";
 import { useAuth } from "../../store/hooks";
-import { useDeleteUserMutation } from "../../pages/userApi";
-import { useNavigate } from "react-router-dom";
-import { useSendLogoutMutation } from "../../pages/authApi";
+import { FooterProvider } from "../Footer/FooterContext";
 
 const Layout = () => {
   const { id: userId } = useAuth();
-  const [deleteUser] = useDeleteUserMutation();
-  const [sendLogout] = useSendLogoutMutation();
-  const navigate = useNavigate();
-
-  const handleDeleteAccount = async () => {
-    try {
-      if (
-        userId &&
-        window.confirm(
-          "Are you sure you want to delete your account? This action cannot be undone.",
-        )
-      ) {
-        await deleteUser(userId).unwrap();
-        await sendLogout({}).unwrap();
-        navigate("/");
-      }
-    } catch (err) {
-      console.error("Failed to delete account:", err);
-    }
-  };
 
   const sidebarNavRoutes: SidebarNavRoute[] = [
     {
@@ -94,15 +71,6 @@ const Layout = () => {
             protected: true,
             regExp: new RegExp("\\/users"),
           },
-          {
-            type: "collapse" as const,
-            nameResourceKey: "Layout:routes.deleteAccount",
-            key: "deleteAccount",
-            noCollapse: true,
-            icon: <DeleteIcon />,
-            protected: true,
-            onClick: handleDeleteAccount,
-          },
         ]
       : [
           {
@@ -117,10 +85,10 @@ const Layout = () => {
         ]),
   ];
   return (
-    <>
+    <FooterProvider>
       <Sidenav color="info" routes={sidebarNavRoutes} />
       <PresistLogin />
-    </>
+    </FooterProvider>
   );
 };
 
