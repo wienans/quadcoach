b349f3f5447568d34e9b256ad0c814fa2a383038
wienans <sven.wienand@outlook.com>
2024-12-01 13:07:03 +0100

    Edit Home Page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -40,6 +40,42 @@ const Home = () => {
                     fontWeight="bold"
                     textTransform="uppercase"
                   >
+                    Favorite Lists and User Profile
+                  </SoftTypography>
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
+                    01.Dec.2024
+                  </SoftTypography>
+                </SoftBox>
+                <SoftTypography>
+                  As new Features i added Favorite Lists for Tacticboards and
+                  Exercises which can be viewed in the new User Profile, where
+                  you also can edit your User Information. For now it is not
+                  pretty but it does its job.
+                  <br />
+                  As a next Feature i am working on letting multiple users edit
+                  the same Tacticboard or Exercise and in case of Private
+                  Tacticboards setting viewer access for users.
+                </SoftTypography>
+              </SoftBox>
+            </Card>
+          </SoftBox>
+          <SoftBox mt={5}>
+            <Card sx={{ height: "100%" }}>
+              <SoftBox p={2}>
+                <SoftBox
+                  display="flex"
+                  justifyContent="space-between"
+                  width="100%"
+                >
+                  <SoftTypography
+                    variant="h3"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                  >
                     Open Beta Testing
                   </SoftTypography>
                   <SoftTypography
