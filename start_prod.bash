0199653dc7f4a24618fd12cf417c1f526e2405ff
wienans <sven.wienand@outlook.com>
2024-12-01 13:27:42 +0100

    improve start prod

--- start_prod.bash
+++ start_prod.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -rf client/dist
+npm --prefix ./client run build
+npm --prefix ./server run build
 docker compose -f docker-compose.dev down
 docker compose -f docker-compose.prod stop
 docker rm quadcoach-mongodb quadcoach-backend quadcoach-frontend
