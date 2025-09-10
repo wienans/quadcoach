787e4815e26f5e5fb5a550843c6169e2376fbc96
wienans <sven.wienand@outlook.com>
2025-09-10 15:24:04 +0200

    Make a production docker build

--- start_prod.bash
+++ start_prod.bash
@@ -1,11 +1,8 @@
 #!/bin/bash
-rm -rf client/dist
-npm --prefix ./client install
-npm --prefix ./server install
-npm --prefix ./client run build
-npm --prefix ./server run build
-docker compose -f docker-compose.dev down
-docker compose -f docker-compose.prod stop
+# set -euo pipefail
+
+docker compose -f docker-compose.dev down 
+docker compose -f docker-compose.prod down
 docker rm quadcoach-mongodb quadcoach-backend quadcoach-frontend
 docker image rm quadcoach-app
 docker compose -f docker-compose.prod up --build -d
