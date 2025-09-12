7a3361c6cf53a97943cb571576f17108dce5e231
wienans <sven.wienand@outlook.com>
2025-09-12 10:28:33 +0200

    revert to env vars

--- start_prod.bash
+++ start_prod.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 # set -euo pipefail
+set -a
 source env.env
 source secret.env
 source mongo.env
+set +a
 docker compose -f docker-compose.dev down 
 docker compose -f docker-compose.prod down
 docker rm quadcoach-mongodb quadcoach-backend quadcoach-frontend
