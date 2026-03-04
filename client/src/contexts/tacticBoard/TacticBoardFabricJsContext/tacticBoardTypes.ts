e9946d5d72b55f65cf7d88648dc70e4454e1a3c2
wienans <sven.wienand@outlook.com>
2026-03-04 19:06:27 +0100

    Hot Fix Lines

--- client/src/contexts/tacticBoard/TacticBoardFabricJsContext/tacticBoardTypes.ts
+++ client/src/contexts/tacticBoard/TacticBoardFabricJsContext/tacticBoardTypes.ts
@@ -10,6 +10,10 @@ export interface TacticBoardObject {
   type: string;
   left?: number;
   top?: number;
+  x1?: number;
+  y1?: number;
+  x2?: number;
+  y2?: number;
   width?: number;
   height?: number;
   fill?: string;
