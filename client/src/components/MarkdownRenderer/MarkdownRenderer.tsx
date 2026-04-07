33e6efdd8f3b04560d159cddbf223576b3599e22
wienans <sven.wienand@outlook.com>
2026-04-07 21:26:13 +0200

    Fix Markdown

--- client/src/components/MarkdownRenderer/MarkdownRenderer.tsx
+++ client/src/components/MarkdownRenderer/MarkdownRenderer.tsx
@@ -1,4 +1,7 @@
-import Markdown from "react-markdown";
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import MDEditor from "@uiw/react-md-editor";
+import "@uiw/react-markdown-preview/markdown.css";
 import remarkGfm from "remark-gfm";
 
 interface MarkdownRendererProps {
@@ -6,7 +9,40 @@ interface MarkdownRendererProps {
 }
 
 const MarkdownRenderer = ({ children }: MarkdownRendererProps) => {
-  return <Markdown remarkPlugins={[remarkGfm]}>{children}</Markdown>;
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        overflowX: "auto",
+        "& .wmde-markdown": {
+          backgroundColor: "transparent",
+          color: "inherit",
+          fontFamily: "inherit",
+        },
+        "& .wmde-markdown > :first-of-type": {
+          mt: 0,
+        },
+        "& .wmde-markdown > :last-child": {
+          mb: 0,
+        },
+        "& .wmde-markdown pre": {
+          overflowX: "auto",
+        },
+        "& .wmde-markdown table": {
+          display: "block",
+          maxWidth: "100%",
+          overflowX: "auto",
+        },
+      }}
+    >
+      <MDEditor.Markdown
+        source={children}
+        remarkPlugins={[remarkGfm]}
+        wrapperElement={{ "data-color-mode": theme.palette.mode }}
+      />
+    </Box>
+  );
 };
 
 export default MarkdownRenderer;
